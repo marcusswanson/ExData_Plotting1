6775cc93b5d42143c40c8ba6871a39e52b8b9be9
Marcus Swanson <marcus.swanson@madebymoonbeam.com>
2015-11-07 18:59:43 +0000

    Plot 4

--- plot4.R
+++ plot4.R
@@ -30,17 +30,30 @@ draw_plot <- function() {
         plotData <- filter(plotData, between(Date, as.Date("2007-02-01"), as.Date("2007-02-02")))
         plotData$Global_active_power_in_kw <- as.double(plotData$Global_active_power)/1000
         
-        ## Figure 3
+        ## Figure 4
         
-        png(filename = "plot3.png", width = 480, height = 480,
+        png(filename = "plot4.png", width = 480, height = 480,
             units = "px", pointsize = 12, bg = "white", res = NA,
             restoreConsole = TRUE)
         
-        with(plotData, plot(DateAndTime, Sub_metering_1, type = "n", xlab="", ylab="Energy sub metering",ylim=c(0, 37)))
-        with(plotData, lines(DateAndTime, Sub_metering_1, col="black"))
-        with(plotData, lines(DateAndTime, Sub_metering_2, col="red"))
-        with(plotData, lines(DateAndTime, Sub_metering_3, col="blue"))
-        legend("topright", lwd = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+        par(mfrow = c(2,2))
+        with(plotData, {
+          
+              plot(DateAndTime, Global_active_power_in_kw, type = "n", xlab="", ylab="Global Active Power (kilowatts)",ylim=c(0, 6))
+              lines(DateAndTime, Global_active_power_in_kw)
+              
+              plot(DateAndTime, as.double(Voltage)/8.1, type = "n", xlab="datetime", ylab="Voltage")
+              lines(DateAndTime, as.double(Voltage)/8.1)
+
+              plot(DateAndTime, Sub_metering_1, type = "n", xlab="", ylab="Energy sub metering",ylim=c(0, 37))
+              lines(DateAndTime, Sub_metering_1, col="black")
+              lines(DateAndTime, Sub_metering_2, col="red")
+              lines(DateAndTime, Sub_metering_3, col="blue")
+              
+              plot(DateAndTime, as.double(Global_reactive_power)/500, type = "n", xlab="datetime", ylab="Global_reactive_power", ylim=c(0,0.5))
+              lines(DateAndTime, as.double(Global_reactive_power)/500)
+        })
+
         dev.off()
 
 }
